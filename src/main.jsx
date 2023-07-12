c8fc19f86539bb8469c84a9e5b3c2ebd66941ced
Sheeba Dola <46590121+Sheebadola@users.noreply.github.com>
2023-07-12 13:19:09 -0500

    Delete src directory

--- src/main.jsx
+++
@@ -1,9 +0,0 @@
-// eslint-disable-next-line no-unused-vars
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-
-const rootElement = document.getElementById('root');
-
-// Use createRoot instead of ReactDOM.render
-ReactDOM.createRoot(rootElement).render(<App />);
